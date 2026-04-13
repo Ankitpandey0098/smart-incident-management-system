fce64c2c8356adc65e4b8e7e2bcccbb61d838309
Ankitpandey0098 <ankitpd308@gmail.com>
2026-04-13 21:43:07 +0530

    fix backend url issue

--- frontend/incident-frontend/src/api/axios.js
+++ frontend/incident-frontend/src/api/axios.js
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api", // your Django API base
+  baseURL:  "https://smart-incident-management-system-chno.onrender.com/api", // your Django API base
   withCredentials: true, // IMPORTANT: allow cookies for session auth
     headers: {
     "Content-Type": "application/json",
