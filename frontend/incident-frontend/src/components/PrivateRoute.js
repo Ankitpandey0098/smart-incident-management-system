3e8104847bb9589d01a4ed7137891001a5032cc2
Ankitpandey0098 <ankitpd308@gmail.com>
2026-04-14 14:33:29 +0530

    update private route

--- frontend/incident-frontend/src/components/PrivateRoute.js
+++ frontend/incident-frontend/src/components/PrivateRoute.js
@@ -1,7 +1,19 @@
 import { Navigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const PrivateRoute = ({ children }) => {
-  const token = localStorage.getItem("access");
+  const [token, setToken] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const storedToken = localStorage.getItem("access");
+    setToken(storedToken);
+    setLoading(false);
+  }, []);
+
+  if (loading) {
+    return null; // wait until token loads
+  }
 
   if (!token) {
     return <Navigate to="/login" replace />;
