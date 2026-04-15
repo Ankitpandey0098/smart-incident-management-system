e59670efd4859ff658efb9d6756c45241912ff7a
Ankitpandey0098 <ankitpd308@gmail.com>
2026-04-15 14:25:09 +0530

    fix userContext file

--- frontend/incident-frontend/src/components/PrivateRoute.js
+++ frontend/incident-frontend/src/components/PrivateRoute.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { UserContext } from "../UserContext";
 
+
 const PrivateRoute = ({ children }) => {
 
   const { loading } = useContext(UserContext);
